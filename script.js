83338efd7dc92415003c2b7cc3e306755726948d
dulnithLiyanage <dulnithliyanage2@gmail.com>
2021-12-13 11:03:35 +0530

    Completely the functionality of the calculator (using eval)

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
-const operatorName = {
-  "+": "add",
-  "-": "subtract",
-  "*": "multiply",
-  "/": "divide",
-  "%": "modulus",
-};
+// ** Variables --------------------------------------------------------------------------------------------------------------------
 
-function add(num1, num2) {
-  return (num1 * 100 + num2 * 100) / 100;
-}
+const calculationDisplay = document.querySelector("#calculation-display");
+const numberDisplay = document.querySelector("#number-display");
+const clearEntry = document.querySelector("#clear-entry");
+const clearAll = document.querySelector("#clear-all");
+const equalSign = document.querySelector("#equal");
+const backspace = document.querySelector("#backspace");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
 
-function subtract(num1, num2) {
-  return (num1 * 100 - num2 * 100) / 100;
-}
+numbers.forEach((number) => {
+  number.addEventListener("click", function () {
+    numberDisplay.textContent += number.textContent;
+  });
+});
 
-function multiply(num1, num2) {
-  return (num1 * 100 * (num2 * 100)) / (100 * 100);
-}
+operators.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    while (numberDisplay.textContent != "") {
+      let num = parseFloat(numberDisplay.textContent);
+      if (operator.textContent != "=") {
+        calculationDisplay.textContent += `${num} ${operator.textContent} `;
+      } else {
+        calculationDisplay.textContent += `${num} `;
+      }
+      numberDisplay.textContent = "";
+    }
+  });
+});
 
-function divide(num1, num2) {
-  return (((num1 * 100) / num2) * 100) / (100 * 100);
-}
-
-function modulus(num1, num2) {
-  return (((num1 * 100) % num2) * 100) / 100;
-}
-
-function operate(operator) {
-  return operatorName[operator];
-}
-
-let num1 = parseFloat(prompt("Enter the first number"));
-let operator = prompt("add(+), subtract(-), multiply(*), divide(/) ");
-let num2 = parseFloat(prompt("Enter the second number"));
-
-function returnResult() {
-  let isFunction = window[operate(operator)];
-
-  if (typeof isFunction === "function") {
-    console.log(isFunction(num1, num2));
+equalSign.addEventListener("click", function () {
+  let answer = 0;
+  if (isFloat(answer) === true) {
+    answer = eval(calculationDisplay.textContent);
+  } else {
+    answer = eval(calculationDisplay.textContent);
   }
-}
+  calculationDisplay.textContent = "";
+  numberDisplay.textContent = answer;
+});
 
-returnResult();
+clearAll.addEventListener("click", function () {
+  calculationDisplay.textContent = "";
+  numberDisplay.textContent = "";
+});
+
+clearEntry.addEventListener("click", function () {
+  numberDisplay.textContent = "";
+});
+
+backspace.addEventListener("click", function () {
+  let numbers = numberDisplay.textContent.split("");
+  numbers.pop(numbers[numbers.length - 1]);
+  numberDisplay.textContent = numbers.join("");
+});
+
+function isFloat(n) {
+  return Number(n) === n && n % 1 !== 0;
+}
